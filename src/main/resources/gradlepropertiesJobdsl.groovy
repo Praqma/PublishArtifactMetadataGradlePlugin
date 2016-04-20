658a88aeb6a765d959c0667b38315171df759836
Alexandra Sedova <als@praqma.net>
2016-04-20 12:54:11 +0200

    Add review job with configure pretested plugin

--- src/main/resources/gradlepropertiesJobdsl.groovy
+++ src/main/resources/gradlepropertiesJobdsl.groovy
@@ -1,5 +1,5 @@
-def myJob = freeStyleJob('GradlePropertiesCheck')
-myJob.with {
+def publishJob = freeStyleJob('GradlePropertiesPublish')
+publishJob.with {
     description 'This is job for Gradle upload artifactory properties project'
     scm{
         git {
@@ -16,6 +16,39 @@ myJob.with {
             tasks('artifactorypublish')
             useWrapper(true)
             fromRootBuildScriptDir(true)
+        }
+    }
+}
+
+def reviewJob = freeStyleJob('GradlePropertiesReview')
+reviewJob.with {
+    description 'This is job for review new changes in Gradle artifactory properties project'
+    scm{
+        git {
+            remote {
+                name('origin')
+                url('TBD')
+            }
+            branch('*/ready/**')
+        }
+    }
+    steps{
+        gradle{
+            tasks('clean')
+            tasks('test')
+            useWrapper(true)
+            fromRootBuildScriptDir(true)
+        }
+    }
+    configure{ project ->
+        project / publishers << 'org.jenkinsci.plugins.pretestedintegration.PretestedIntegrationPostCheckout' {}
+        project / buildWrappers << 'org.jenkinsci.plugins.pretestedintegration.PretestedIntegrationBuildWrapper' {
+            scmBridge('class': 'org.jenkinsci.plugins.pretestedintegration.scm.git.GitBridge') {
+                branch 'master'
+                integrationStrategy('class': 'org.jenkinsci.plugins.pretestedintegration.scm.git.SquashCommitStrategy')
+                repoName 'origin'
+            }
+            rollbackEnabled false
         }
     }
 }
